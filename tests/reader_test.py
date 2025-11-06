eab06e4ac90d7c85dd8462546923253952d54629
sudini <sudini@gamma.ph.liv.ac.uk>
2025-11-06 17:34:43 +0000

    Moved test script to tests directory

---
+++ tests/reader_test.py
@@ -0,0 +1,17 @@
+import VX274X_unpacker
+import matplotlib.pyplot as plt
+
+dir = "/bundle/data/DarkSide/phaidaq/run01942"
+
+mreader= VX274X_unpacker.MIDASreader(dir)
+for i,event in enumerate(mreader):
+    # print(f'Event # {i}\t#Modules {event.nboards}\t#Channels {event.nchannels}\t#Samples {event.nsamples}')
+    # print(f'\tchannel mask {bin(event.channel_mask)[2:]}\ttrigger time: {event.trigger_time}')
+    # access waveforms in event
+    wfs = event.adc_data # shape = (number of channels, number of waveform sample)
+    
+    if len(wfs)==0:
+        pass
+    else:
+        plt.plot(wfs[0])
+        plt.show()
