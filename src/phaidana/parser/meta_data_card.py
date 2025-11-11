a61bfac13c4fc267263076a20a1c6cea0bb2c5f3
sudini <sudini@gamma.ph.liv.ac.uk>
2025-11-11 12:00:34 +0000

    Added meta_data_card.py for parser

---
+++ src/phaidana/parser/meta_data_card.py
@@ -0,0 +1,24 @@
+import midas.file_reader as fr 
+
+# Open our file
+mfile = fr.MidasFile("/bundle/data/DarkSide/phaidaq/run02038/run02038sub0000.mid")
+
+# We can simply iterate over all events in the file
+# for event in mfile:
+#     bank_names = ", ".join(b.name for b in event.banks.values())
+#     print("Event # %s of type ID %s contains banks %s" % (event.header.serial_number, event.header.event_id, bank_names))
+   
+odb = mfile.get_bor_odb_dump().data   
+
+###### Run metadata
+run_number = odb["Runinfo"]["Run number"]
+metadata = odb["Experiment"]["Edit on Start"]
+
+#print(f'This run is run{run_number} and was taken by {metadata["shifter"]}')
+#print(f'This run was {metadata["run type"]} for {metadata["comment"]}')
+
+event = mfile.read_this_event_body()
+print(event.header.event_id)
+
+
+
