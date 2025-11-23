ec31342e8df5488567cdfe30801d1500368720fe
sudini <sudini@theta.ph.liv.ac.uk>
2025-11-23 15:40:11 +0000

    Updated test script

--- tests/reader_test.py
+++ tests/reader_test.py
@@ -2,13 +2,11 @@ import numpy as np
 import phaidana.parser.pyreader.VX274X_unpacker as unpack
 from scipy.signal import find_peaks
 from phaidana.analysis.filter import Filter
-from phaidana.analysis.pulse_finder import Pulse
+import phaidana.analysis.pulse_finder as pf
 
 import matplotlib.pyplot as plt
 
 filter = Filter()
-pulse = Pulse()
-
 dir = "/bundle/data/DarkSide/phaidaq/run01906"
 
 mreader= unpack.MIDASreader(dir)
@@ -18,16 +16,36 @@ for i,event in enumerate(mreader):
     # access waveforms in event
     wfs = event.adc_data # shape = (number of channels, number of waveform sample)
     bal = filter.get_baseline(wfs, gate=250, start=0)
-    bal_rms = np.rms(bal)
     wfs_sub = wfs - bal
+    bal_rms = np.sqrt(np.mean(bal**2))
+    bal_std = np.std(bal)
+
     if len(wfs)==0:
         pass
     else:
-        if np.max(wfs[0]) > 55000:
+        if (np.max(wfs[0]) > 55000) or (len(filter.pulse_in_pretrig(wfs_sub ,gate=250, start=0))>0):
             pass
         else:
-            peaks, properties = find_peaks(wfs_sub[0], height=1000)
-            plt.plot(wfs_sub[0]) 
-            plt.plot(peaks, wfs_sub[0][peaks], 'ro')
+
+            # Run Finder
+            config = pf.PulseConfig(high_threshold  = 200.0, 
+                                    low_threshold = 100.0,
+                                    avg_gate = 5,
+                                    min_gap_samples = 10,
+                                    min_after_peak = 100,
+                                    valley_depth_threshold = 2.0,
+                                    valley_rel_fraction = 0.2)
+            finder = pf.PulseFinder(config)
+            pulses,  filtered_trace = finder.process(wfs[0])
+
+            print(pulses)
+            first_pulse = pulses[0]
+            start = first_pulse.t_start
+            end = first_pulse.t_end
+            print(start)
+            plt.plot(wfs_sub[0],color='k')
+            plt.plot(filtered_trace,color='b')
+            plt.axvline(x=start, color='r')
+            plt.axvline(x=end, color='r')
             plt.show()           
 
